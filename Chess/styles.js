a32699d42a389d875c23e3522df84a5c39dbece8
Arpit08-coder-glitch <arpitsin28@gmail.com>
2025-01-22 10:50:18 +0530

    New Project

---
+++ Chess/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: '#ffffff',
+    padding: 10,
+    marginVertical: 10,
+    color: '#ffffff',
+    borderRadius: 5,
+    backgroundColor: '#2e2e2e',
+  },
+  button: {
+    backgroundColor: '#bb86fc',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+});
