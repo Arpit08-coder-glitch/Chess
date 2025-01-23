3862537b976c71b08ff4496b42f0ef94ae2f68e2
Arpit08-coder-glitch <arpitsin28@gmail.com>
2025-01-23 16:43:41 +0530

    android build updates

---
+++ Chess/App copy.js
@@ -0,0 +1,25 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './LoginScreen';
+import SignupScreen from './SignUpScreen';
+import HomeScreen from './HomeScreen';
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        {/* Add a placeholder Home screen for testing */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
